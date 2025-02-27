9a5251dec5487f5d714a0451f1838f14a1db0d94
shuliaka95 <58136995+shuliaka95@users.noreply.github.com>
2025-02-27 11:34:37 +0300

    remake 1.4

---
+++ 1.4b/1.4.cpp
@@ -0,0 +1,32 @@
+﻿#include "func.h"
+
+
+int main() {
+    const double eps = 1e-6;
+
+    std::cout << "|   s   |     x      |    f(x)     | k_iter |\n"
+        << "|-------|------------|-------------|--------|\n";
+
+    // Решение для уравнения а
+    int iter_a;
+    auto func_a = [](double x, double) { return equation_a(x); };
+    double root_a = bisection(func_a, 0, 3, eps, iter_a);
+
+    std::cout << std::fixed << std::setprecision(6)
+        << "|  -    | " << std::setw(10) << root_a
+        << " | " << std::setw(11) << equation_a(root_a)
+        << " | " << std::setw(6) << iter_a << " |\n";
+
+    // Решение для уравнения б
+    for (double s = 1; s <= 3; s += 1) {
+        int iter_b;
+        auto func_b = [](double x, double s) { return equation_b(x, s); };
+        double root_b = bisection(func_b, -1, 0.7, eps, iter_b, s);
+
+        std::cout << "| " << std::setw(5) << s << " | " << std::setw(10) << root_b
+            << " | " << std::setw(11) << equation_b(root_b, s)
+            << " | " << std::setw(6) << iter_b << " |\n";
+    }
+
+    return 0;
+}
--- 1.4b/func.h
+++ 1.4b/func.h
@@ -1,5 +1,42 @@
 #pragma once
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+// Объявления функций
 double equation_a(double x);
 double equation_b(double x, double s);
-double bisection(double (*func)(double), double a, double b, double eps, int& iterations);
-double bisection(double (*func)(double, double), double a, double b, double eps, double s, int& iterations);
+
+// Шаблон метода половинного деления
+template<typename Func>
+double bisection(Func func, double a, double b, double eps, int& iterations, double s = 0) {
+    iterations = 0;
+    double fa = func(a, s);
+    double fb = func(b, s);
+
+    if (fa * fb > 0) {
+        std::cout << "[Ошибка] Функция не меняет знак на интервале ["
+            << a << ", " << b << "]\n";
+        return NAN;
+    }
+
+    double mid, fmid;
+    do {
+        mid = (a + b) / 2;
+        fmid = func(mid, s);
+        iterations++;
+
+        if (std::abs(fmid) < eps) break;
+
+        if (fa * fmid < 0) {
+            b = mid;
+            fb = fmid;
+        }
+        else {
+            a = mid;
+            fa = fmid;
+        }
+    } while (std::abs(b - a) > eps);
+
+    return mid;
+}
